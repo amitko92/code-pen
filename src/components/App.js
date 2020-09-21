a3a3097e134790a12c761a58e0408bcd69113a24
Amit Koren <30484152+amitko92@users.noreply.github.com>
2020-09-21 19:23:45 +0300

    commiting

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from './Editor';
 
 
 function App() {
-  return "Hello world"
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [javaScript, setJavaScript] = useState('')
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor 
+          language='xml'
+          displayName='HTML'
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+          language='css'
+          displayName='CSS'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor 
+          language='javascript'
+          displayName='JavaScript'
+          value={javaScript}
+          onChange={setJavaScript}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        />
+      </div>
+    </>
+  )
 }
 
 export default App;
